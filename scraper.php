175cc131dd51258e3e5bd74388aeb8b19787bc5d
arasabbasi <aras.abbasi@googlemail.com>
2016-01-26 12:43:26 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -43,7 +43,8 @@ function scrapePOST($url) {
 
 $url = "http://motavafian.beheshtm.ir/peaplesearch.php";
 
-$content = scrapePost($url);
+$context  = stream_context_create($opts);
+$content = file_get_contents($url, false, $context, -1, 40000);
 
 preg_match_all("/<td class=\"alt2\" align=\"right\">(\d*)<\/td>.*<td class=\"alt1\" align=\"right\">(.*)<\/td>.*<td class=\"alt2\" align=\"right\">(.*)<\/td>.*<td class=\"alt1\" align=\"right\">(.*)<\/td>.*<td class=\"alt2\" align=\"right\">(.*)<\/td>.*<td class=\"alt2\" align=\"right\">(\d*)<\/td>.*<td class=\"alt1\" align=\"right\">(\d*)<\/td>.*<td class=\"alt2\" align=\"right\">(\d*)<\/td>.*<td class=\"alt1\" align=\"right\">(\d*)<\/td>.*<td class=\"alt2\" align=\"right\">(.*)<\/td>/Usmi", $content, $output_array);
 
